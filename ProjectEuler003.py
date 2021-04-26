bbc87e8d8296061ac9cd4f8da131685eaf1b0835
Joshua Appleby <82772500+JoshuaAppleby@users.noreply.github.com>
2021-04-26 23:04:05 +0100

    Added time and answer

--- ProjectEuler003.py
+++ ProjectEuler003.py
@@ -5,6 +5,8 @@
     What is the largest prime factor of the number 600851475143 ?
 """
 
+import time
+
 def largestprimefactor(N):
     i = 2
     n = N
@@ -14,4 +16,7 @@ def largestprimefactor(N):
         i = i+1
     return n
 
+start = time.time()
 print(largestprimefactor(600851475143))
+print(time.time() - start)
+#6857 in 0.0009632110595703125 secs

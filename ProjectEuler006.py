0ee03fa7bbad9fd1b713fe336a1a57a145820dbf
Joshua Appleby <82772500+JoshuaAppleby@users.noreply.github.com>
2021-04-26 23:05:53 +0100

    Added time and answer

--- ProjectEuler006.py
+++ ProjectEuler006.py
@@ -1,14 +1,16 @@
 #Project Euler, Problem 6
 #Sum square difference
+"""The sum of the squares of the first ten natural numbers is,
+    1**2 + 2**2 + ... + 10**2 = 385
+    The square of the sum of the first ten natural numbers is,
+    (1+2+...+10)**2 = 55**2 = 3025
+    Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025-385=2640
+    Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+"""
+
+import time
 
 def sumdif(N):
-    """The sum of the squares of the first ten natural numbers is,
-        1**2 + 2**2 + ... + 10**2 = 385
-        The square of the sum of the first ten natural numbers is,
-        (1+2+...+10)**2 = 55**2 = 3025
-        Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025-385=2640
-        Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-    """
     sumofsquares = 0
     sumnat = 0
     for i in range(1,N+1):
@@ -18,4 +20,7 @@ def sumdif(N):
     sumsquare = sumnat**2
     return sumsquare - sumofsquares
 
+start = time.time()
 print(sumdif(100))
+print(time.time() - start)
+#25164150 in 0.0009062290191650391 secs
